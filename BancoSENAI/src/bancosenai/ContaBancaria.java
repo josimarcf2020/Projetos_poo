b1a129c7a24c2de7ad42ad09a608a5f61256aa19
Josimar da Cunha Ferreira <62252790+josimarcf2020@users.noreply.github.com>
2023-05-17 13:56:14 -0300

    Projeto Banco SENAI

---
+++ BancoSENAI/src/bancosenai/ContaBancaria.java
@@ -0,0 +1,28 @@
+
+package bancosenai;
+
+public class ContaBancaria {
+    //atributos
+    String agencia;
+    String conta;
+    Double saldo;
+    String titular;
+    
+    //metodos
+    void depositar(){
+        
+    }
+    
+    void sacar(){
+        
+    }
+    
+    void transferir(){
+        
+    }
+    
+    String consultarSaldo(){
+        return "";
+    }
+    
+}
